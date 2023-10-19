a19bf1f97c633fa0a4e6581321eb51ed579181d1
verifit <verifit@fit.vutbr.cz>
2023-10-19 14:29:34 +0200

    Update build scripts

--- build-python.sh
+++ build-python.sh
@@ -2,3 +2,6 @@
 basedir=$(realpath $(dirname "$0"))
 echo "[!] Building Python interpret"
 make -C "$basedir/src/python" install
+if [ $? -ne 0 ]; then
+  exit 1
+fi
--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 basedir=$(realpath $(dirname "$0"))
 echo "[!] Building C++ interpret"
+pushd $basedir
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake ..  -DCMAKE_BUILD_TYPE=Release
 make
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
 cp ./src/cpp/*-interpreter ~/bin
+popd
 echo "[done]"
